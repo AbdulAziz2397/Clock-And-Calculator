95a08f558eeedeb742f1b41721e7c149392a58ec
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2025-04-13 21:30:07 +0500

    completed

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 
+
+
+
 setInterval( function(){
     var clock = new Date();
-    
+
     var hr = document.getElementById("hr");
     var min = document.getElementById("min");
     var sec = document.getElementById("sec");
-    
+    var month = document.getElementById("month");
+    var day = document.getElementById("day");
+    var year = document.getElementById("year");
+    var date = document.getElementById("date");
+    var ampm = document.getElementById("ampm");
+
     var hours = clock.getHours();
     var minutes = clock.getMinutes();
     var seconds = clock.getSeconds();
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var days = ['Sunday,', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,'];
+    var years = clock.getFullYear();
+    var dates = clock.getDate();
     
+    if(hours > 11){
+        ampm.innerText = 'PM';
+        hours = hours - 12;
+    }
+
     hr.innerText = hours;
     min.innerText = minutes;
     sec.innerText = seconds;
-    
-}, 0000)
+    month.innerText = months[clock.getMonth()];
+    day.innerText = days[clock.getDay()];
+    year.innerText = years;
+    date.innerText = dates;
+
+}, 10);
