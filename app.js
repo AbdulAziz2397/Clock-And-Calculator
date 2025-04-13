d75d8d6e6616fdbfdc9c28c502992b77173b10ef
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2025-04-14 02:31:30 +0500

    completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 
-setInterval( function(){
+setInterval(function () {
     var clock = new Date();
 
     var hr = document.getElementById("hr");
@@ -14,24 +14,27 @@ setInterval( function(){
     var date = document.getElementById("date");
     var ampm = document.getElementById("ampm");
 
-    var hours = clock.getHours()+1;
+    var hours = clock.getHours() + 1;
     var minutes = clock.getMinutes();
     var seconds = clock.getSeconds();
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var days = ['Sunday,', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,'];
     var years = clock.getFullYear();
     var dates = clock.getDate();
-    
-    if(hours >= 12){
+
+    if (hours >= 12) {
         ampm.innerText = 'PM';
-        if(hours > 12){
-            hours = hours - 12;
-        }
     }
-    else{
+    else {
         ampm.innerText = 'AM';
     }
-    
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    else {
+        hours = hours - 1;
+    }
+
     hr.innerText = String(hours);
     min.innerText = String(minutes).padStart(2, '0');
     sec.innerText = String(seconds).padStart(2, '0');
@@ -39,18 +42,19 @@ setInterval( function(){
     day.innerText = days[clock.getDay()];
     year.innerText = years;
     date.innerText = dates;
-    
-    if(seconds.length < 2){
+
+    if (seconds.length < 2) {
         sec.innerText = String("0") + seconds;
     }
 }, 1000);
 
-function rotateScreen (){
+function rotateScreen() {
     var myClock = document.getElementById('myclock');
-    if(myClock.style.transform === 'rotate(90deg)'){
+
+    if (myClock.style.transform === 'rotate(90deg)') {
         myClock.style.transform = 'rotate(0deg)'
     }
-    else{
+    else {
         myClock.style.transform = 'rotate(90deg)';
     }
 }
